16bb8addd2ef1e3e6f9e4f1a9b9dab96502bc2d6
Paul Patault <paul.patault@universite-paris-saclay.fr>
2024-02-09 22:00:41 +0100

    added keyboard and i3lock

---
+++ i3lock.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BLANK='#175057bb'
+CLEAR='#f2e5bc22'
+DEFAULT='#076678cc'
+TEXT='#ebdbb2ee'
+WRONG='#fb4934bb'
+VERIFYING='#fabd2fbb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"    \
+--keylayout 1                \
